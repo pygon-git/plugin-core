3a360154e7e3542284e3b0cc70401cae86a2e7fd
Brian French <fr3nch13@gmail.com>
2017-09-22 16:14:12 -0700

    Initial addition of the bootstrap plugin.

---
+++ config/bootstrap.php
@@ -0,0 +1,14 @@
+<?php
+
+use Cake\Core\Configure;
+use Cake\Core\Configure\Engine\PhpConfig;
+use Cake\Core\Plugin;
+
+try {
+    Configure::config('PluginCore', new PhpConfig());
+    Configure::load('app', 'PluginCore', false);
+} catch (\Exception $e) {
+    exit($e->getMessage() . "\n");
+}
+
+Plugin::load('BootstrapUI');
