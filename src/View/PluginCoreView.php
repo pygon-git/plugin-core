2ff838b7b8d22847392f40c9e012bfc1c8878058
Brian French <fr3nch13@gmail.com>
2017-09-23 15:46:47 -0700

    Added the basic layout templates.

---
+++ src/View/PluginCoreView.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace PygonGit\PluginCore\View;
+
+use Cake\View\View;
+use BootstrapUI\View\UIView;
+
+class PluginCoreView extends UIView
+{
+    public function initialize()
+    {
+        parent::initialize();
+    }
+}
