05707e395174bf0096a16f490e1c48faa6b88e44
Brian French <fr3nch13@gmail.com>
2017-09-23 18:41:42 -0700

    Added the ability to link the vendor folder to webroot/assets.

---
+++ src/Console/PluginCoreInstaller.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ * @link      https://cakephp.org CakePHP(tm) Project
+ * @since     3.0.0
+ * @license   https://opensource.org/licenses/mit-license.php MIT License
+ */
+namespace PygonGit\PluginCore\Console;
+
+use Cake\Utility\Security;
+use Composer\Script\Event;
+use Exception;
+
+/**
+ * Provides installation hooks for when this application is installed via
+ * composer. Customize this class to suit your needs.
+ */
+class PluginCoreInstaller
+{
+
+    /**
+     * Does some routine installation tasks so people don't have to.
+     *
+     * @param \Composer\Script\Event $event The composer event object.
+     * @throws \Exception Exception raised by validator.
+     * @return void
+     */
+    public static function postInstall(Event $event)
+    {
+        static::linkVendorAssets($event);
+    }
+
+    public static function postUpdate(Event $event)
+    {
+        $io = $event->getIO();
+
+        static::linkVendorAssets($event);
+
+        $io->write('PygonGit\PluginCore is updated.');
+    }
+
+    /**
+     * links the vendor assets to the webroot for the css/js files
+     *
+     * @param string $dir The application's root directory.
+     * @param \Composer\Script\Event $event
+     * @return void
+     */
+    public static function linkVendorAssets(Event $event)
+    {
+        $io = $event->getIO();
+        $io->write('Checking Vendor Assets');
+
+        require "config".DIRECTORY_SEPARATOR."paths.php";
+
+        $isLinked = false;
+
+        if ($handle = opendir(WWW_ROOT))
+        {
+
+            while (false !== ($entry = readdir($handle)))
+            {
+                if($entry == 'assets')
+                    $isLinked = true;
+            }
+
+            closedir($handle);
+        }
+
+        if($isLinked)
+        {
+            $io->write('Vendor Assets already linked');
+        }
+        else
+        {
+            $io->write('Linking Vendor Assets');
+            $io->write(ROOT);
+
+            symlink(ROOT.DS.'vendor', WWW_ROOT.DS.'assets');
+        }
+
+    }
+}
