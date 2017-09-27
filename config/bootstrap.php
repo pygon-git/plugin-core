3214d31933bee5961cc21172f6ebeec21fb6edab
Brian French <fr3nch13@gmail.com>
2017-09-27 16:11:24 -0700

    Added fontawesome for the icon library.

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -11,6 +11,14 @@ try {
     exit($e->getMessage() . "\n");
 }
 
+/*
+ * Only try to load DebugKit in development mode
+ * Debug Kit should not be installed on a production system
+ */
+if (Configure::read('debug')) {
+    Plugin::load('DebugKit', ['bootstrap' => true]);
+}
+
 Plugin::load('Migrations');
 Plugin::load('BootstrapUI');
 Plugin::load('Cake/ElasticSearch', ['bootstrap' => true]);
