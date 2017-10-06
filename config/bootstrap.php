2d995f5e144910781270f21398a8785ff0d06eba
Brian French <fr3nch13@gmail.com>
2017-10-06 16:07:43 -0700

    Initial integration with CakeDC/Users plugin

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -22,3 +22,24 @@ if (Configure::read('debug')) {
 Plugin::load('Migrations');
 Plugin::load('BootstrapUI');
 Plugin::load('Cake/ElasticSearch', ['bootstrap' => true]);
+
+// User management from the CakeDC 
+Configure::write('Users.config', ['users']);
+Plugin::load('CakeDC/Users', ['routes' => true, 'bootstrap' => true]);
+
+Configure::write('Auth.authenticate.Form.fields.username', 'email'); // use their email instead of a username
+
+// allow social logins // All of the keays are test keys and route to a non-public website
+Configure::write('Users.Social.login', true); //to enable social login
+Configure::write('Users.GoogleAuthenticator.login', true);
+Configure::write('OAuth.providers.facebook.options.clientId', '536639270007034');
+Configure::write('OAuth.providers.facebook.options.clientSecret', 'ade8b402cb34fae5c24f6978631bdfd1');
+
+Configure::write('OAuth.providers.twitter.options.clientId', '18nzzMJ3dEUbJO2HgClcDMLU8');
+Configure::write('OAuth.providers.twitter.options.clientSecret', 'wKrfiZAnCaORzFpKy8wQAiTo7XRNfpqVDSlibQoMAqk4MKpq6Q');
+
+// Enable recaptcha
+Configure::write('Users.reCaptcha.key', '6LeEgDMUAAAAADbL8LWGIOeSJDD7opS4BV3-KlO-');
+Configure::write('Users.reCaptcha.secret', '6LeEgDMUAAAAAA7On51ITALhomGC_mbctQBOiXuh');
+Configure::write('Users.reCaptcha.registration', true); //enable on registration
+Configure::write('Users.reCaptcha.login', true); //enable on login
